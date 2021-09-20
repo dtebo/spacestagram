306dba931570eff706838aa142639b95458ef3e5
dtebo <darrentebo83@outlook.com>
2021-09-20 14:29:37 -0400

    added nasa api call to retreive images

--- src/components/Images/ImageList.js
+++ src/components/Images/ImageList.js
@@ -6,14 +6,22 @@ const ImageList = (props) => {
     const [images, setImages] = useState([]);
 
     useEffect(() => {
-
+        axios.get("https://api.nasa.gov/planetary/apod?start_date=2021-01-01&end_date=2021-09-01&api_key=hujeiE0i5EMgKa1KAjMzpnaKbARkFafgwpfWsWFx")
+            .then(photos => {
+                console.log(photos);
+                setImages(photos.data);
+            })
+            .catch(err => console.error(err));
     }, []);
 
     return(
         <>
             <h1>Image list goes here...</h1>
             {images && images.map(image => {
-                return(<ImageCard Image={image}/>)
+                return(<ImageCard 
+                            key={image.url}
+                            Image={image}
+                        />)
             })}
         </>
     );
